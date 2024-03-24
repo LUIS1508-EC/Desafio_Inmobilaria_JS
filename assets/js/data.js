cd7bd54ff58dd827cdc10b40e5a3b802603257d2
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-03-24 14:33:03 -0600

    JS

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+const propiedadesJson = [
+    {
+        nombre:"Apartamento de lujo en zona exclusiva",
+        descripcion:"Este apartamento de lujo está ubicado en una exclusiva zona residencial.",
+        src:"https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+        cuartos:"3",
+        metros:"75"
+    },
+    {
+        nombre:"Moderna casa en el sur de Chile",
+        descripcion:"Casa en los bosques del sur de Chile con acceso a todas las comodidades modernas.",
+        src:"https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+        cuartos:"5",
+        metros:"150"
+    },
+    {
+        nombre:"Refugio en la montaña",
+        descripcion:"Hermoso y rústico refugio de montaña para grupos grandes de aventureros",
+        src:"https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        cuartos:"10",
+        metros:"250"
+    },
+    {
+        nombre:"Casa en el Cajón del Maipo",
+        descripcion:"Casa familiar en el Cajón del Maipo, cerca de Santiago y desconectada de la realidad.",
+        src:"https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+        cuartos:"3",
+        metros:"75"
+    },
+    {
+        nombre:"Casa al borde de la playa",
+        descripcion:"Hermosa casa con terraza al borde de la playa para vacacionar.",
+        src:"https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        cuartos:"4",
+        metros:"100"
+    },
+    {
+        nombre:"Moderna Casa en la playa",
+        descripcion:"Moderna casa al lado de la playa para que puedas surfear a cualquier hora.",
+        src:"https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+        cuartos:"6",
+        metros:"180"
+    },
+]
